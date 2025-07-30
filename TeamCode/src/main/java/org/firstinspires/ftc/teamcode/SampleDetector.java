313b9f14a04e4700271f1ef8ff5e1aee699094b1
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2025-07-30 15:10:41 -0500

    init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleDetector.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class SampleDetector extends LinearOpMode {
+
+    Limelight3A limelight;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LimelightConstants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+public class LimelightConstants {
+    public static final int BLUE_PIPELINE = 0;
+    public static final int RED_PIPELINE = 1;
+    public static final int YELLOW_PIPELINE = 2;
+}
